2652e5ca7e8d8d651bcf83f3e3b31faa184a888f
Nipul Mallikarachchi <nipulm03@gmail.com>
2026-02-08 19:16:24 +0530

    Bug fixes

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -3,12 +3,14 @@ package generator
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/NipulM/oisbase/internal/prompts"
 	"github.com/NipulM/oisbase/internal/utils"
+	"github.com/NipulM/oisbase/templates"
 )
 
 func GenerateReadme(config *prompts.ProjectConfig) {
@@ -37,52 +39,43 @@ func GenerateReadme(config *prompts.ProjectConfig) {
 	fmt.Println("README.md generated successfully")
 }
 
-func CopyModules(config *prompts.ProjectConfig) {
-	modulesDir := "templates/services/aws/modules"
-	modules, err := os.ReadDir(modulesDir)
-	if err != nil {
-		log.Fatalf("Failed to read modules directory: %v", err)
-	}
-
-	selectedSet := make(map[string]bool)
-	for _, s := range config.Services {
-		selectedSet[s] = true
-	}
-
-	for _, module := range modules {
-		if !module.IsDir() {
-			continue
-		}
-
-		if !selectedSet[module.Name()] {
-			continue
-		}
-
-		moduleDir := filepath.Join(modulesDir, module.Name())
-		moduleFiles, err := os.ReadDir(moduleDir)
-		if err != nil {
-			log.Fatalf("Failed to read module directory: %v", err)
-		}
-
-		outDir := filepath.Join("modules", module.Name())
-		if err := os.MkdirAll(outDir, 0755); err != nil {
-			log.Fatalf("Failed to create output directory %s: %v", outDir, err)
-		}
-
-		for _, moduleFile := range moduleFiles {
-			if moduleFile.IsDir() {
-				continue
-			}
-			moduleFilePath := filepath.Join(moduleDir, moduleFile.Name())
-			moduleFileContent, err := os.ReadFile(moduleFilePath)
-			if err != nil {
-				log.Fatalf("Failed to read module file: %v", err)
-			}
-
-			outPath := filepath.Join(outDir, moduleFile.Name())
-			if err := os.WriteFile(outPath, moduleFileContent, 0644); err != nil {
-				log.Fatalf("Failed to write %s: %v", outPath, err)
-			}
-		}
-	}
+func CopyModules(config *prompts.ProjectConfig) error {
+    modulesBaseDir := "services/aws/modules"
+    
+    for _, service := range config.Services {
+        serviceDir := filepath.Join(modulesBaseDir, service)
+        
+        // Read from embedded filesystem
+        entries, err := fs.ReadDir(templates.ModulesFS, serviceDir)
+        if err != nil {
+            fmt.Printf("Warning: No module template found for %s, skipping\n", service)
+            continue
+        }
+        
+        // Create destination directory
+        destDir := filepath.Join("modules", service)
+        if err := os.MkdirAll(destDir, 0755); err != nil {
+            return fmt.Errorf("failed to create module directory: %w", err)
+        }
+        
+        // Copy each file from embedded FS to destination
+        for _, entry := range entries {
+            if entry.IsDir() {
+                continue
+            }
+            
+            srcPath := filepath.Join(serviceDir, entry.Name())
+            content, err := templates.ModulesFS.ReadFile(srcPath)
+            if err != nil {
+                return fmt.Errorf("failed to read embedded file %s: %w", srcPath, err)
+            }
+            
+            destPath := filepath.Join(destDir, entry.Name())
+            if err := os.WriteFile(destPath, content, 0644); err != nil {
+                return fmt.Errorf("failed to write file %s: %w", destPath, err)
+            }
+        }
+    }
+    
+    return nil
 }
---
+++ templates/templates.go
@@ -0,0 +1,6 @@
+package templates
+
+import "embed"
+
+//go:embed all:services
+var ModulesFS embed.FS
